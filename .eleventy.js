81526ae28cd7611a7e1641489cd7b1a5028bc48a
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2022-04-15 18:34:46 +0300

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function (eleventyConfig) {
+
+    const { DateTime } = require("luxon");
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/index.js');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+}
---
+++ public/index.js
@@ -0,0 +1,23 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i,
+        tabContent,
+        tablinks;
+    // Get all elements with class="tab__content" and hide them
+    tabContent = document.getElementsByClassName("tab__content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document
+        .getElementById(tabName)
+        .style
+        .display = "block";
+    evt.currentTarget.className += " active";
+}
